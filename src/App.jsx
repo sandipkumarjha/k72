072711779eb966c17d5d57d503c19e9667ecc1be
Sandip kumar jha <sandipkumarjha211@gmail.com>
2026-04-15 03:00:56 +0530

    projectsection

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import Navbar from './components/Navigation/Navbar'
+import FullScreenNav from './components/Navigation/FullScreenNav'
 
 
 const App = () => {
 
   return (
-    <div className='text-white'>
-
-        <Navbar/> 
+    <div className='text-white overflow-x-hidden'>
+        
+        <Navbar/>
+        <FullScreenNav/> 
         <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/agence' element={<Agence/>}/>
